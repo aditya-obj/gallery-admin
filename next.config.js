9ca2a6e2feea3c65a95ea2f077a2a4f85a6e164e
aditya-obj <adityaakumar.in@gmail.com>
2025-05-14 13:52:57 +0530

    allow images and videos from anywhere

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'images.unsplash.com', 
-      'via.placeholder.com', 
-      'firebasestorage.googleapis.com', 
-      'rukminim1.flixcart.com',
-      'rukminim2.flixcart.com',
-      'rukminim3.flixcart.com',
-      'rukminim4.flixcart.com',
-      'rukminim5.flixcart.com',
-      'm.media-amazon.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      }
     ],
   },
 }
